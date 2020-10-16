3fff0583fdecb0530c36ef3acdf1aa71a1b27fb7
NobleWolf12 <51687911+NobleWolf12@users.noreply.github.com>
2020-10-15 21:13:54 -0300

    Update connectBanco.php

--- connectBanco.php
+++ connectBanco.php
@@ -2,7 +2,7 @@
     $servername = "localhost";
     $database="db_lista";
     $username="root";
-    $password="@Wolf@98";
+    $password="";
 
 
     $link = mysqli_connect($servername, $username, $password,$database);
